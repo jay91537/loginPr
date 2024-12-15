98689c3e22bdfa6882dc4608b58efc9555fe4cba
jay91537 <veuxft@gmail.com>
2024-12-15 23:53:02 +0900

    Feat : first commit

---
+++ src/test/java/com/example/hello_login/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
